7e3a16f1b3853254cbca1818bbd8dc14a27db120
lumoko27 <lumoko2@gmail.com>
2024-01-30 17:19:49 -0300

    aula final

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -1,3 +1,5 @@
+const converteIds = require('../utils/conversorDeStringHelper.js');
+
 class Controller {
   constructor(entidadeService) {
     this.entidadeService = entidadeService;
@@ -8,7 +10,7 @@ class Controller {
       const listaDeRegistro = await this.entidadeService.pegaTodosOsRegistros();
       return res.status(200).json(listaDeRegistro);
     } catch (erro) {
-      // erro
+      return res.status(500).json({erro: erro.message});
     }
   }
 
@@ -18,7 +20,18 @@ class Controller {
       const umRegistro = await this.entidadeService.pegaUmRegistroPorId(Number(id));
       return res.status(200).json(umRegistro);
     } catch (erro) {
-      // erro
+      return res.status(500).json({erro: erro.message});
+    }
+  }
+
+  async pegaUm(req, res) {
+    const { ...params } = req.params;
+    const where = converteIds(params);
+    try {
+      const umRegistro = await this.entidadeService.pegaUmRegistro(where);
+      return res.status(200).json(umRegistro);
+    } catch (erro) {
+      return res.status(500).json({erro: erro.message});
     }
   }
 
@@ -28,22 +41,23 @@ class Controller {
       const novoRegistroCriado = await this.entidadeService.criaRegistro(dadosParaCriacao);
       return res.status(200).json(novoRegistroCriado);
     } catch (erro) {
-      // erro
+      return res.status(500).json({erro: erro.message});
     }
   }
 
   async atualiza(req, res) {
-    const { id } = req.params;
+    const { ...params } = req.params;
     const dadosAtualizados = req.body;
+
+    const where = converteIds(params);
     try {
-      //isUpdated
-      const foiAtualizado = await this.entidadeService.atualizaRegistro(dadosAtualizados, Number(id));
+      const foiAtualizado = await this.entidadeService.atualizaRegistro(dadosAtualizados, where);
       if (!foiAtualizado) {
         return res.status(400).json({ mensagem: 'registro não foi atualizado' });
       }
       return res.status(200).json({ mensagem: 'Atualizado com sucesso' });
     } catch (erro) {
-      // erro
+      return res.status(500).json({erro: erro.message});
     }
   }
 
@@ -52,8 +66,8 @@ class Controller {
     try {
       await this.entidadeService.excluiRegistro(Number(id));
       return res.status(200).json({ mensagem: `id ${id} deletado` });
-    } catch (error) {
-      // erro
+    } catch (erro) {
+      return res.status(500).json({erro: erro.message});
     }
   }
 }
--- src/controllers/CursoController.js
+++ src/controllers/CursoController.js
@@ -1,3 +1,5 @@
+const { Op } = require('sequelize');
+
 const Controller = require('./Controller.js');
 const CursoServices = require('../services/CursoServices.js');
 
@@ -7,6 +9,29 @@ class CursoController extends Controller {
   constructor() {
     super(cursoServices);
   }
+
+  async pegaCursos (req, res) {
+    const { data_inicial, data_final } = req.query;
+    const where = {};
+
+    // se existir os param, criar uma prop {}
+    data_inicial || data_final ? where.data_inicio = {} : null;
+
+    // se existir data inicial, adiciona a prop gte com o valor
+    data_inicial ? where.data_inicio[Op.gte] = data_inicial : null;
+
+    //se existir data final, adiciona a prop lte com o valor
+    data_final ? where.data_inicio[Op.lte] = data_final : null;
+
+    try {
+      const listaCursos = await cursoServices.pegaTodosOsRegistros(where);
+      return res.status(200).json(listaCursos);
+    }
+    catch (erro) {
+      return res.status(500).json({ erro: erro.message });
+    }
+  }
+
 }
 
 module.exports = CursoController;
